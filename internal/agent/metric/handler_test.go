fb83c2c69d4a838f42ca729e73400d3d7bc294f3
Nikolai Zhogin <nickzhog@gmail.com>
2022-12-12 13:52:49 +0300

    unit tests for agent

--- internal/agent/metric/handler_test.go
+++ internal/agent/metric/handler_test.go
@@ -1,39 +1,59 @@
 package metric
 
 import (
-	"fmt"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_getFloat(t *testing.T) {
 	tests := []struct {
-		name  string
-		input interface{}
+		name       string
+		input      interface{}
+		wantResult float64
 	}{
 		{
-			name:  "test number-string",
-			input: "1.1",
+			name:       "test number-string",
+			input:      "1.1",
+			wantResult: 0,
 		},
 		{
-			name:  "string",
-			input: "test string",
+			name:       "string",
+			input:      "test string",
+			wantResult: 0,
 		},
 		{
-			name:  "string-number",
-			input: "3.1 ",
+			name:       "string-number",
+			input:      "3.1 ",
+			wantResult: 0,
 		},
 		{
-			name:  "int",
-			input: 4,
+			name:       "int",
+			input:      4,
+			wantResult: 4,
+		},
+		{
+			name:       "int64",
+			input:      int64(123),
+			wantResult: 123,
+		},
+		{
+			name:       "float64",
+			input:      float64(33.1231),
+			wantResult: 33.1231,
+		},
+		{
+			name:       "float32",
+			input:      float32(423.1231),
+			wantResult: float64(float32(423.1231)),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
 
-			result, ok := getFloat(tt.input)
-			fmt.Printf("getFloat() input:%v,type:%v, result:%v, ok:%v\n",
-				tt.input, reflect.TypeOf(tt.input), result, ok)
+			result, _ := getFloat(tt.input)
+			require.Equal(result, tt.wantResult, tt.name)
 		})
 	}
 }
--- internal/agent/metric/metric_test.go
+++ internal/agent/metric/metric_test.go
@@ -1,25 +1,100 @@
 package metric
 
-import "testing"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
 
-func TestMetrics_InitMetrics(t *testing.T) {
-	type fields struct {
-		GaugeMetrics   map[string]float64
-		CounterMetrics map[string]int64
-	}
+	"github.com/jarcoal/httpmock"
+	"github.com/nickzhog/practicum-metric/internal/agent/config"
+	serverMetric "github.com/nickzhog/practicum-metric/internal/server/metric"
+	"github.com/nickzhog/practicum-metric/pkg/logging"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetrics_SendMetrics(t *testing.T) {
+	var storage Metrics
+	storage.InitMetrics()
+	cfg := &config.Config{}
+	logger := logging.GetLogger()
+	cfg.Settings.Address = "http://localhost"
+
 	tests := []struct {
-		name   string
-		fields fields
+		name    string
+		metrics Metrics
 	}{
-		// TODO: Add test cases.
+		{
+			name: "case #1",
+			metrics: Metrics{
+				CounterMetrics: map[string]int64{
+					"good_counter": 10,
+				},
+			},
+		},
+		{
+			name: "case #2",
+			metrics: Metrics{
+				GaugeMetrics: map[string]float64{
+					"good_gauge": 15.51,
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := &Metrics{
-				GaugeMetrics:   tt.fields.GaugeMetrics,
-				CounterMetrics: tt.fields.CounterMetrics,
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+			mockStorage := serverMetric.NewMemStorage()
+
+			httpmock.RegisterResponder(http.MethodPost, "http://localhost/update",
+				func(req *http.Request) (*http.Response, error) {
+					body, err := io.ReadAll(req.Body)
+					if err != nil {
+						return httpmock.NewStringResponse(http.StatusBadRequest, ""), err
+					}
+
+					var elem serverMetric.Metrics
+					err = json.Unmarshal(body, &elem)
+					if err != nil {
+						return httpmock.NewStringResponse(http.StatusBadRequest, ""), err
+					}
+					var value interface{}
+					switch elem.MType {
+					case serverMetric.CounterType:
+						mockStorage.UpdateCounterElem(elem.ID, *elem.Delta)
+						value = *elem.Delta
+					case serverMetric.GaugeType:
+						mockStorage.UpdateGaugeElem(elem.ID, *elem.Value)
+						value = *elem.Value
+					default:
+						return httpmock.NewStringResponse(http.StatusBadRequest, ""), err
+					}
+
+					resp, err := httpmock.NewJsonResponse(200,
+						serverMetric.MetricToJSON(elem.ID, elem.MType, value))
+					if err != nil {
+						return httpmock.NewStringResponse(http.StatusBadGateway, ""), err
+					}
+					return resp, nil
+				})
+
+			//////////
+			assert := assert.New(t)
+
+			storage.CounterMetrics = tt.metrics.CounterMetrics
+			storage.GaugeMetrics = tt.metrics.GaugeMetrics
+
+			storage.SendMetrics(cfg, logger)
+
+			assert.Equal(len(storage.CounterMetrics)+len(storage.GaugeMetrics),
+				httpmock.GetTotalCallCount())
+			if len(storage.CounterMetrics) > 0 {
+				assert.Equal(storage.CounterMetrics, mockStorage.FindAll().CounterMetrics)
 			}
-			m.InitMetrics()
+			if len(storage.GaugeMetrics) > 0 {
+				assert.Equal(storage.GaugeMetrics, mockStorage.FindAll().GaugeMetrics)
+			}
 		})
 	}
 }
