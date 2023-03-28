9fb8308a30adc85968c774fbbe753af15cfea085
Nikolai Zhogin <nickzhog@gmail.com>
2023-03-28 12:40:42 +0300

    migrations moved

--- migration/migrations/001_table.up.sql
+++ migration/migrations/001_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.metrics (
-    id text not null, 
-    type text not null,
-    value double precision,
+    id TEXT NOT NULL, 
+    type TEXT NOT NULL,
+    value DOUBLE PRECISION,
     delta BIGINT,
     PRIMARY KEY (id, type)
 );
